e72f3170c11dc3f9c24682e9b3c09870c9b27fb1
MNKRCT <muhammedkrct@gmail.com>
2025-08-03 13:16:56 +0300

    Variables and Datatypes egzersizi

---
+++ VariablesDatatypes.py
@@ -0,0 +1,6 @@
+urun_adi = "Bilgisayar"
+urun_fiyat = 10862.42
+urun_stok = 268
+print("UrunAdi: " + urun_adi)
+print("Urun Fiyati: " + str(urun_fiyat))
+print("Urun Stok Sayisi: " + str(urun_stok))
