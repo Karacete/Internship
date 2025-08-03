cd9823fbdf6bb96b1cd7f18e78302461fc178cf3
MNKRCT <muhammedkrct@gmail.com>
2025-08-03 13:29:04 +0300

    User Input egzersizi

---
+++ UserInput.py
@@ -0,0 +1,4 @@
+adi = input("Adinizi giriniz: ")
+km = float(input("Kaç kilometre gittiniz? "))
+mile = km * 1.609
+print("Merhaba " + adi + ", " + str(km) + " kilometre " + str(mile) + " mil eder.")
