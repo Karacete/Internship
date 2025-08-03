5d0c105b41d889d29533eacdc073e69a2208d0df
MNKRCT <muhammedkrct@gmail.com>
2025-08-03 14:11:19 +0300

    Strings egzersizi

---
+++ Strings.py
@@ -0,0 +1,5 @@
+msg='welcome to Python 101: Strings'
+print(msg)
+new_msg = msg[18] +' '+ msg[:7] +' '+ msg[25:29] +' '+ msg[7:10] +' '+ msg[13] + msg[12] + msg[2] + msg[1] + msg[-5]
+print (new_msg.title())
+print(new_msg[::-1].title())
