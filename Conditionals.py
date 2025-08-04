2a266ff7d4ca833c4319f21039ace62ed73a767e
MNKRCT <muhammedkrct@gmail.com>
2025-08-04 10:17:14 +0300

    Conditionals egzersizi

---
+++ Conditionals.py
@@ -0,0 +1,9 @@
+def num_days(month):
+    days = 31
+    if month in ('apr','jun','sep','nov'):
+        days = 30
+    elif month == 'feb':
+        days = 28
+    print('number of days in',month,'is',days)
+
+num_days('jun')
